08280070705f6f5b1340de7acf7b6fa9ef008917
Jon de Andres <jondeandres@gmail.com>
2013-05-06 01:02:58 +0200

    Add basic RedisModel class, it provides:
    - attributes saved in hash format for the object key
    - counters
    - relation collections

---
+++ redis-moon.lua
@@ -0,0 +1,227 @@
+local redis = require('redis')
+local client = redis.connect('127.0.0.1', 6379)
+local RedisModel
+do
+  local _parent_0 = nil
+  local _base_0 = {
+    counter_key_for = function(self, counter)
+      return self:key() .. ':' .. counter
+    end,
+    get_counter = function(self, counter)
+      local value = client:get(self:counter_key_for(counter))
+      return value or 0
+    end,
+    inc_counter = function(self, counter)
+      return client:incrby(self:counter_key_for(counter), 1)
+    end,
+    decr_counter = function(self, counter)
+      return client:decrby(self:counter_key_for(counter), 1)
+    end,
+    field_key = function(self, field)
+      return self:key() .. ":" .. field
+    end,
+    collection = function(self, name)
+      local collection = { }
+      local collection_ids = client:smembers(self:field_key(name))
+      local relation_class = self.__class:relation_class_for('collections', name)
+      local _list_0 = collection_ids
+      for _index_0 = 1, #_list_0 do
+        local id = _list_0[_index_0]
+        local item = relation_class:get(id)
+        table.insert(collection, item)
+      end
+      return collection
+    end,
+    add_to_collection = function(self, collection, id)
+      return client:sadd(self:field_key(collection), id)
+    end,
+    remove_from_collection = function(self, collection, id)
+      return client:srem(self:field_key(collection), id)
+    end,
+    spawn_methods = function(self)
+      local __index = getmetatable(self).__index
+      local _list_0 = self.__class.counters
+      for _index_0 = 1, #_list_0 do
+        local counter = _list_0[_index_0]
+        local name = counter .. '_count'
+        __index[name] = function()
+          return self:get_counter(counter)
+        end
+        name = 'incr_' .. counter
+        __index[name] = function()
+          return self:inc_counter(counter)
+        end
+        name = 'decr_' .. counter
+        __index[name] = function()
+          return self:decr_counter(counter)
+        end
+      end
+      for collection, model_fn in pairs(self.__class.collections) do
+        __index[collection] = function(self)
+          return self:collection(collection)
+        end
+        local name = 'add_' .. collection
+        __index[name] = function(self, id)
+          return self:add_to_collection(collection, id)
+        end
+        name = 'remove_' .. collection
+        __index[name] = function(self, id)
+          return self:remove_from_collection(collection, id)
+        end
+      end
+    end,
+    key = function(self)
+      self._key = self._key or self.__class:object_key(self.id)
+      return self._key
+    end,
+    save = function(self)
+      if not self.id then
+        self.__class:increment_counter()
+        self.id = self.__class:counter()
+      end
+      return client:hmset(self:key(), self:attributes())
+    end,
+    attributes = function(self)
+      local tbl = { }
+      local _list_0 = self.__class.attrs
+      for _index_0 = 1, #_list_0 do
+        local attr = _list_0[_index_0]
+        tbl[attr] = self[attr]
+      end
+      return tbl
+    end
+  }
+  _base_0.__index = _base_0
+  if _parent_0 then
+    setmetatable(_base_0, _parent_0.__base)
+  end
+  local _class_0 = setmetatable({
+    __init = function(self, attributes)
+      self:spawn_methods()
+      if attributes then
+        for attr, value in pairs(attributes) do
+          self[attr] = value
+        end
+      end
+    end,
+    __base = _base_0,
+    __name = "RedisModel",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil and _parent_0 then
+        return _parent_0[name]
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  local self = _class_0
+  self.primary_key = "id"
+  self.model = nil
+  self.counters = { }
+  self.counter = function(self)
+    return client:get(self:counter_key())
+  end
+  self.increment_counter = function(self)
+    return client:incrby(self:counter_key(), 1)
+  end
+  self.relation_class_for = function(self, type, relation)
+    return self[type][relation]()
+  end
+  self.counter_key = function(self)
+    self._counter_key = self._counter_key or self.model .. ":count"
+    return self._counter_key
+  end
+  self.object_key = function(self, id)
+    return self.model .. ":" .. self.primary_key .. ":" .. id
+  end
+  self.get = function(self, id)
+    local tbl = client:hgetall(self:object_key(id))
+    tbl.id = id
+    return self(tbl)
+  end
+  self.create = function(self, attributes)
+    local item = self(attributes)
+    item:save()
+    return item
+  end
+  if _parent_0 and _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  RedisModel = _class_0
+end
+do
+  local _parent_0 = RedisModel
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  if _parent_0 then
+    setmetatable(_base_0, _parent_0.__base)
+  end
+  local _class_0 = setmetatable({
+    __init = function(self, ...)
+      if _parent_0 then
+        return _parent_0.__init(self, ...)
+      end
+    end,
+    __base = _base_0,
+    __name = "User",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil and _parent_0 then
+        return _parent_0[name]
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  local self = _class_0
+  self.model = "users"
+  self.attrs = {
+    'name'
+  }
+  self.counters = {
+    'tweets'
+  }
+  self.collections = {
+    followers = function()
+      return User
+    end
+  }
+  if _parent_0 and _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  User = _class_0
+end
+local user1 = User()
+user1.name = 'Bob'
+user1:save()
+local user2 = User({
+  name = 'Alice'
+})
+user2:save()
+local user3 = User:create({
+  name = 'Trudy'
+})
+user1:incr_tweets()
+user1:incr_tweets()
+user1:incr_tweets()
+print(user1:tweets_count())
+user1:add_followers(user2.id)
+user1:add_followers(user3.id)
+return print(#user1:followers())
---
+++ redis-moon.moon
@@ -0,0 +1,126 @@
+redis = require('redis')
+client = redis.connect('127.0.0.1', 6379)
+
+class RedisModel
+  @primary_key: "id"
+  @model: nil
+  @counters: {}
+
+-- class methods
+  @counter: =>
+    client\get(@counter_key!)
+
+  @increment_counter: =>
+    client\incrby(@counter_key!, 1)
+
+  @relation_class_for: (type, relation) =>
+    @[type][relation]!
+
+  @counter_key: =>
+    @_counter_key = @_counter_key or @model .. ":count"
+    @_counter_key
+
+  @object_key: (id) =>
+    @model .. ":" .. @primary_key .. ":" .. id
+
+  @get: (id) =>
+    tbl = client\hgetall(@\object_key(id))
+    tbl.id = id
+    @(tbl)
+  @create: (attributes) =>
+    item = @(attributes)
+    item\save!
+    item
+
+-- instance methods
+  new: (attributes) =>
+    @spawn_methods!
+
+    if attributes
+      for attr, value in pairs attributes
+        @[attr] = value
+
+  counter_key_for: (counter) =>
+    @\key! .. ':' .. counter
+
+  get_counter: (counter) =>
+    value = client\get(@\counter_key_for(counter))
+    value or 0
+
+  inc_counter: (counter) =>
+    client\incrby(@\counter_key_for(counter), 1)
+
+  decr_counter: (counter) =>
+    client\decrby(@\counter_key_for(counter), 1)
+
+  field_key: (field) =>
+    @\key! .. ":" .. field
+
+  collection: (name) =>
+    collection = {}
+    collection_ids = client\smembers(@\field_key(name))
+    relation_class = @@\relation_class_for('collections', name)
+
+    for id in *collection_ids
+      item = relation_class\get(id)
+      table.insert(collection, item)
+    collection
+
+  add_to_collection: (collection, id) =>
+    client\sadd(@\field_key(collection), id)
+
+  remove_from_collection: (collection, id) =>
+    client\srem(@\field_key(collection), id)
+
+  spawn_methods: =>
+    __index = getmetatable(@).__index
+
+    -- spawn counter methods
+    for counter in *@@counters
+      -- count
+      name = counter .. '_count'
+      __index[name] = ->
+        @\get_counter counter
+
+      -- inc
+      name = 'incr_' .. counter
+      __index[name] = ->
+        @\inc_counter(counter)
+
+      -- decr
+      name = 'decr_' .. counter
+      __index[name] = ->
+        @\decr_counter counter
+    -- spawn collection methods
+    for collection, model_fn in pairs @@collections
+      -- the collection method
+      __index[collection] =  (self) ->
+        @\collection(collection)
+
+      -- add method
+      name = 'add_' .. collection
+      __index[name] = (self, id) ->
+        @\add_to_collection(collection, id)
+
+      -- remove method
+      name = 'remove_' .. collection
+      __index[name] = (self, id) ->
+        @\remove_from_collection(collection, id)
+
+  key: =>
+      @_key = @_key or @@\object_key(@id)
+      @_key
+
+  save: =>
+    if not @id
+      @@increment_counter!
+      @id = @@counter!
+    client\hmset(@key!, @\attributes!)
+
+  attributes: =>
+    tbl = {}
+
+    for attr in *@@attrs
+      tbl[attr] = @[attr]
+    tbl
+
